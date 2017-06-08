5210ccd3527698df6e2af952370c5fc68e294ff5
ke li <forestlii@outlook.com>
2017-06-08 17:07:24 +0800

    Create a Quiz Game 2 From Unity OverWrite By Forestli

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class DataController : MonoBehaviour 
+{
+	private RoundData[] allRoundData;
+	private PlayerProgress playerProgress;
+	private string gameDataFileName = "data.json";
+	void Start ()  
+	{
+		DontDestroyOnLoad (gameObject);
+		LoadGameData();
+		LoadPlayerProgress();
+		SceneManager.LoadScene ("MenuScreen");
+	
+	}
+	
+	public RoundData GetCurrentRoundData()
+	{
+		return allRoundData [0];
+	}
+	public void SubmitNewPlayerScore(int newScore)
+	{
+		if (newScore > playerProgress.highestScore)
+		{
+			playerProgress.highestScore = newScore;
+			SavePlayerProgress();
+		}
+	}
+	public int GetHighestPlayerScore()
+	{
+		return playerProgress.highestScore;
+	}
+	private void LoadPlayerProgress()
+	{
+		playerProgress = new PlayerProgress();
+		if (PlayerPrefs.HasKey("highestScore"))
+		{
+			playerProgress.highestScore= PlayerPrefs.GetInt("highestScore");
+		}
+	}
+	private void SavePlayerProgress()
+	{
+		PlayerPrefs.SetInt("highestScore",playerProgress.highestScore);
+	}
+	private void LoadGameData()
+	{
+		string filePath = Path.Combine(Application.streamingAssetsPath,gameDataFileName);
+		Debug.Log(filePath);
+		if (File.Exists(filePath))
+		{
+			string dataAsJson = File.ReadAllText(filePath);
+			//Debug.Log(dataAsJson);
+			GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+			allRoundData = loadedData.allRoundData;
+
+		}
+		else
+		{
+			Debug.LogError("Cannot load game data!");
+		}
+	}
+}
